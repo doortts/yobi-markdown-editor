641b9c86009d6f53b1d8b31874b1f88a4e9517df
Suwon Chae <doortts@gmail.com>
2015-07-21 17:55:08 +0900

    editor: Introduce editor directive

--- app/app.js
+++ app/app.js
@@ -3,13 +3,34 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
       'ngRoute',
-      'myApp.view1',
-      'myApp.view2',
       'myApp.version'
     ]).
     config(['$routeProvider', function($routeProvider) {
       $routeProvider.otherwise({redirectTo: '/view1'});
     }])
-    .controller('EditorCtrl', function($scope){
+    .controller('EditorCtrl', ['$scope', function($scope){
+        $scope.john = { title: "John Bill"};
+    }])
+    .directive('yobiEditor',function(){
+        // Return the directive configuration.
+        return ({
+            link: link,
+            templateUrl: "editor.html",
+            restrict: 'E',
+            scope: {
+                editor: '='
+            }
+        });
 
+        function link( scope, element, attributes ) {
+            $(function(){
+                $('.editor-contents').atwho({
+                    at: "@",
+                    data:['npcode', 'changsung', 'doortts']
+                }).atwho({
+                    at: "#",
+                    data: ["#1. 첫번째 이슈", "#2. 두번째 이슈", "#3. 세번째 이슈"]
+                });
+            });
+        }
     });
