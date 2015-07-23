24d0b77c3efbce3e16fc908f91b2ac8ce21c1774
Suwon Chae <doortts@gmail.com>
2015-07-23 16:19:50 +0900

    editor: Decouple mention data

--- app/app.js
+++ app/app.js
@@ -8,27 +8,50 @@ angular.module('myApp', [
     config(['$routeProvider', function($routeProvider) {
       $routeProvider.otherwise({redirectTo: '/view1'});
     }])
-    .controller('EditorCtrl', ['$scope', function($scope){
-        $scope.john = { title: "John Bill"};
+    .service("editorService", function(){
+        var mention = {
+            users: ['npcode', 'changsung', 'doortts'],
+            issues: ["#1. 첫번째 이슈", "#2. 두번째 이슈", "#3. 세번째 이슈"]
+        }
+        return {
+            users: function(){
+                return mention.users;
+            },
+            issues: function(){
+                return mention.issues;
+            }
+        }
+    })
+    .controller('EditorCtrl', ['$scope', 'editorService', function($scope, editorService){
+        $scope.getUsers = function(){
+            return editorService.users();
+        };
+        $scope.getIssues = function () {
+            return editorService.issues();
+        }
     }])
-    .directive('yobiEditor',function(){
+    .directive('yobiEditor', function(){
         // Return the directive configuration.
+
         return ({
             link: link,
             templateUrl: "editor.html",
             restrict: 'E',
             scope: {
-                editor: '='
+                editor: '=',
+                usermention: '=',
+                issuemention: '='
             }
         });
 
         function link( scope, element, attributes ) {
+            console.log('hello', scope.usermention, 'go');
             $(element).find('.editor-contents').atwho({
                 at: "@",
-                data:['npcode', 'changsung', 'doortts']
+                data: scope.usermention
             }).atwho({
                 at: "#",
-                data: ["#1. 첫번째 이슈", "#2. 두번째 이슈", "#3. 세번째 이슈"]
+                data: scope.issuemention
             });
         }
     });
