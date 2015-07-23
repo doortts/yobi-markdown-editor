0a13fc80530a17b258503414997ad56bc7b03b3f
Suwon Chae <doortts@gmail.com>
2015-07-23 16:34:21 +0900

    editor: Revamped editor

--- app/app.js
+++ app/app.js
@@ -6,52 +6,27 @@ angular.module('myApp', [
       'myApp.version'
     ]).
     config(['$routeProvider', function($routeProvider) {
-      $routeProvider.otherwise({redirectTo: '/view1'});
     }])
-    .service("editorService", function(){
-        var mention = {
+    .controller('EditorCtrl', ['$scope', function($scope){
+        $scope.mention = {
             users: ['npcode', 'changsung', 'doortts'],
             issues: ["#1. 첫번째 이슈", "#2. 두번째 이슈", "#3. 세번째 이슈"]
         }
-        return {
-            users: function(){
-                return mention.users;
-            },
-            issues: function(){
-                return mention.issues;
-            }
-        }
-    })
-    .controller('EditorCtrl', ['$scope', 'editorService', function($scope, editorService){
-        $scope.getUsers = function(){
-            return editorService.users();
-        };
-        $scope.getIssues = function () {
-            return editorService.issues();
-        }
     }])
     .directive('yobiEditor', function(){
-        // Return the directive configuration.
-
         return ({
             link: link,
             templateUrl: "editor.html",
             restrict: 'E',
-            scope: {
-                editor: '=',
-                usermention: '=',
-                issuemention: '='
-            }
+            scope: true
         });
-
         function link( scope, element, attributes ) {
-            console.log('hello', scope.usermention, 'go');
             $(element).find('.editor-contents').atwho({
                 at: "@",
-                data: scope.usermention
+                data: scope.mention.users
             }).atwho({
                 at: "#",
-                data: scope.issuemention
+                data: scope.mention.issues
             });
         }
     });
