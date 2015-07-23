5f8d4882c5c883184b0df1b0dc64668a343cffdc
Suwon Chae <doortts@gmail.com>
2015-07-23 18:34:44 +0900

    editor: Add service layer & attr default value

--- app/app.js
+++ app/app.js
@@ -6,11 +6,31 @@ angular.module('myApp', [
       'myApp.version'
     ]).
     config(['$routeProvider', function($routeProvider) {
+      $routeProvider.otherwise({redirectTo: '/view1'});
     }])
-    .controller('EditorCtrl', ['$scope', function($scope){
-        $scope.mention = {
+    .service("editorService", function(){
+        var mention = {
             users: ['npcode', 'changsung', 'doortts'],
             issues: ["#1. 첫번째 이슈", "#2. 두번째 이슈", "#3. 세번째 이슈"]
+        }
+        return {
+            users: function(){
+                return mention.users;
+            },
+            issues: function(){
+                return mention.issues;
+            }
+        }
+    })
+    .controller('EditorCtrl', ['$scope', 'editorService', function($scope, editorService){
+        $scope.user =  editorService.users();
+        $scope.issueMention = editorService.issues();
+        $scope.getUsers = function(){
+            return editorService.users();
+        }
+        $scope.mention = {
+            users: editorService.users(),
+            issues: editorService.issues()
         }
     }])
     .directive('yobiEditor', function(){
@@ -18,15 +38,33 @@ angular.module('myApp', [
             link: link,
             templateUrl: "editor.html",
             restrict: 'E',
-            scope: true
+            scope: {
+                mentions: "@",
+                action: "@",
+                method: "@",
+                userMention: "&",
+                issueMention: "&"
+            },
+            compile: function(element, attrs){
+                if (!attrs.method){
+                    attrs.method = "POST";
+                }
+            }
         });
-        function link( scope, element, attributes ) {
-            $(element).find('.editor-contents').atwho({
-                at: "@",
-                data: scope.mention.users
-            }).atwho({
-                at: "#",
-                data: scope.mention.issues
-            });
+
+        function link( scope, element, attributes) {
+            var editorContents = $(element).find('.editor-contents');
+            if(scope.mentions && scope.mentions.indexOf('@') > -1){
+                editorContents.atwho({
+                    at: "@",
+                    data: scope.userMention()
+                })
+            }
+            if(scope.mentions && scope.mentions.indexOf('#') > -1){
+                editorContents.atwho({
+                    at: "#",
+                    data: scope.issueMention()
+                })
+            }
         }
     });
